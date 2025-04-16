0b7eb0f85f9ac81b2a3be5abd2767db13645d880
M1ST-hub <143425337+M1ST-hub@users.noreply.github.com>
2025-04-16 10:51:41 -0500

    spawns

---
+++ Assets/scrip/Timer.cs
@@ -0,0 +1,94 @@
+using UnityEngine;
+using Unity.Netcode;
+using TMPro;
+
+public class Timer : NetworkBehaviour
+{
+    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] float remainingTime;
+
+    public GameManager gm;
+    static public bool gameStart;
+    static public bool gameEnd;
+    static public bool isSpawned;
+    public bool preGameTimer;
+    public bool postGameTimer;
+    public bool gameInProgress;
+
+    public NetworkVariable<float> Clock = new NetworkVariable<float>();
+
+    private void Start()
+    {
+
+        gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isSpawned == true)
+            CountdownRpc();
+    }
+
+    [Rpc(SendTo.Everyone)]
+    private void CountdownRpc()
+    {
+
+        //countdown timer
+        if (Clock.Value > 0 && IsServer)
+            Clock.Value -= Time.deltaTime;
+        //stop timer at 0
+        else if (Clock.Value < 1 && IsServer)
+        {
+            Clock.Value = 0;
+
+            if (preGameTimer == true && gameStart == false)
+            {
+                gm.GameStartRpc();
+                NetworkObject.Destroy(gameObject);
+            }
+            else if (preGameTimer == false && postGameTimer == false && gameInProgress == true)
+            {
+                gameInProgress = false;
+                gm.GameEndRpc();
+                gameEnd = true;
+                NetworkObject.Destroy(gameObject);
+            }
+            else if (postGameTimer == true && gameEnd == true)
+            {
+                gameEnd = false;
+                gm.GameRestartRpc();
+                NetworkObject.Destroy(gameObject);
+            }
+
+        }
+
+
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        Clock.OnValueChanged += OnStateChange;
+
+        isSpawned = true;
+
+
+        if (IsServer)
+        {
+            Clock.Value = 20;
+        }
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        Clock.OnValueChanged -= OnStateChange;
+    }
+
+    public void OnStateChange(float previous, float current)
+    {
+        int minutes = Mathf.FloorToInt(Clock.Value / 60);
+        int seconds = Mathf.FloorToInt(Clock.Value % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+}
